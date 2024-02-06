c8e7093730c6370dd524fcc4fe8314649cb14eba
guifabri <erickscala7025@gmail.com>
2024-02-06 19:11:12 -0400

    changes

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -23,13 +23,44 @@ function subtractNumbers() {
 
 document.getElementById('subtractNumbers').onclick = subtractNumbers;
 /* Arrow Function - Multiply Numbers */
+const multiplyNumbers = () => {
+  let factor1 = parseFloat(document.getElementById('factor1').value);
+  let factor2 = parseFloat(document.getElementById('factor2').value);
+  let product = factor1 * factor2;
+  document.getElementById('product').value = product;
+}
 
+document.getElementById('multiplyNumbers').addEventListener('click', multiplyNumbers);
 /* Open Function Use - Divide Numbers */
+function divideNumbers() {
+  let dividend = parseFloat(document.getElementById('dividend').value);
+  let divisor = parseFloat(document.getElementById('divisor').value);
+  let quotient = dividend / divisor;
+  document.getElementById('quotient').value = quotient;
+};
+document.getElementById('divideNumbers').addEventListener('click', divideNumbers);
 /* Decision Structure */
-/* ARRAY METHODS - Functional Programming */
+document.getElementById('getTotal').addEventListener('click', function(){
+  let getTotalValue = parseFloat(document.getElementById('subtotal').value);
+  let getcheckBox = document.getElementById('member');
+  if (getcheckBox.checked) {
+    getTotalValue -= getTotalValue * 20/100
+  }
+  document.getElementById('total').textContent = `$ ${getTotalValue.toFixed(2)}`});
+
+  /* ARRAY METHODS - Functional Programming */
+
+let numbersArray = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+
 /* Output Source Array */
+document.getElementById('array').textContent = numbersArray;
 /* Output Odds Only Array */
+document.getElementById('odds').textContent = numbersArray.filter(value=>value%2 !=0);
 /* Output Evens Only Array */
+document.getElementById('evens').textContent = numbersArray.filter(value=>value%2 ==0);
 /* Output Sum of Org. Array */
+document.getElementById('sumOfArray').textContent = numbersArray.reduce((sum, number) => sum + number);
 /* Output Multiplied by 2 Array */
+document.getElementById('multiplied').textContent = numbersArray.map(number => number * 2);
 /* Output Sum of Multiplied by 2 Array */
+document.getElementById('sumOfMultiplied').textContent = (numbersArray.map(number => number * 2)).reduce((sum, number) => sum + number);
