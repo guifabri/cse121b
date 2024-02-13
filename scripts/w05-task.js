4b27f4bdad5160b8207ef2c991b664e145f722b7
guifabri <erickscala7025@gmail.com>
2024-02-13 19:55:23 -0400

    changes

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -2,36 +2,74 @@
 
 /* Declare and initialize global variables */
 
-const templesElement = document.createElement('div');
+const templesElement = document.querySelector("#temples");
+
+var templeList = [];
 
 /* async displayTemples Function */
 
-let = templelist = [];
+const displayTemples = (temples) => {
+  temples.forEach((temple) => {
+    let article = document.createElement("article");
+
+    let h3 = document.createElement("h3");
+    h3.textContent = temple.templeName;
+
+    let img = document.createElement("img");
+    img.src = temple.imageUrl;
+    img.alt = temple.location;
+
+    article.appendChild(h3);
+    article.appendChild(img);
+
+    templesElement.appendChild(article);
+  });
+};
 
 /* async getTemples Function using fetch()*/
 
-const displayTemples = (temples) => {
-    temples.forEach(temple => {
-        let article = document.createElement ('article');
-                
-        let h3 = document.createElement('h3');
-        h3.textContent = temple.templeName;
-        
-        let img = document.createElement('img');
-        img.src = temple.imageUrl;
-        img.alt = temple.location;
-        
-        article.appendChild(h3)
-        article.appendChild('img')
-    });
-}
+const getTemples = async () => {
+  const response = await fetch(
+    "https://byui-cse.github.io/cse121b-ww-course/resources/temples.json"
+  );
+  templeList = await response.json();
+  displayTemples(templeList);
+};
 
 /* reset Function */
 
+const reset = () => {
+  templesElement.innerHTML = "";
+};
 
 /* filterTemples Function */
 
+const filterTemples = function (temples = []) {
+  reset();
+  let filter = document.querySelector("#filtered").value;
+  let templeValue;
+  if (filter == "utah") {
+    templeValue = temples.filter((temple) =>
+      temple.location.toLowerCase().includes("utah")
+    );
+  } else if (filter == "notutah") {
+    templeValue = temples.filter(
+      (temple) => !temple.location.toLowerCase().includes("utah")
+    );
+  } else if (filter == "older") {
+    templeValue = temples.filter(
+      (temple) => new Date(temple.dedicated) < new Date(1950, 0, 1)
+    );
+  } else {
+    templeValue = temples;
+  }
+  displayTemples(templeValue);
+};
 
 getTemples();
 
 /* Event Listener */
+
+document.querySelector("#filtered").addEventListener("change", () => {
+  filterTemples(templeList);
+});
