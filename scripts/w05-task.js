d48982c6752ee6330233125833885cb828c94d45
guifabri <erickscala7025@gmail.com>
2024-02-13 17:33:25 -0400

    changes

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -11,7 +11,19 @@ let = templelist = [];
 /* async getTemples Function using fetch()*/
 
 const displayTemples = (temples) => {
-    
+    temples.forEach(temple => {
+        let article = document.createElement ('article');
+                
+        let h3 = document.createElement('h3');
+        h3.textContent = temple.templeName;
+        
+        let img = document.createElement('img');
+        img.src = temple.imageUrl;
+        img.alt = temple.location;
+        
+        article.appendChild(h3)
+        article.appendChild('img')
+    });
 }
 
 /* reset Function */
