a2c1e884fb4f32a372f697874ea1c2ae485f7787
guifabri <erickscala7025@gmail.com>
2024-02-13 17:21:16 -0400

    changes

---
+++ scripts/w05-task.js
@@ -0,0 +1,25 @@
+/* W05: Programming Tasks */
+
+/* Declare and initialize global variables */
+
+const templesElement = document.createElement('div');
+
+/* async displayTemples Function */
+
+let = templelist = [];
+
+/* async getTemples Function using fetch()*/
+
+const displayTemples = (temples) => {
+    
+}
+
+/* reset Function */
+
+
+/* filterTemples Function */
+
+
+getTemples();
+
+/* Event Listener */
